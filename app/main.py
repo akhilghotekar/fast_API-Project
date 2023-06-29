501d4dab23c9ef4ee91645dc5e319d413fadbc29
Ubuntu <ubuntu@ip-172-31-39-160.ap-south-1.compute.internal>
2023-06-29 18:53:42 +0000

    this is fast_API project

--- app/main.py
+++ app/main.py
@@ -1,35 +1,17 @@
-from typing import List
 from fastapi import FastAPI
-
-from app import services
-from app.schema import UserIn, BaseResponse, UserListOut
 
 app = FastAPI()
 
-
 @app.get("/")
-async def index():
-    """
-    Index route for our application
-    """
-    return {"message": "Hello from FastAPI ;)"}
+def read_root():
+    return {"message": "Hello, world!"}
 
+@app.get("/users")
+def read_users():
+    # Implementation to read users from the JSON file
+    ...
 
-@app.post("/users", response_model=BaseResponse)
-async def user_create(user: UserIn):
-    """
-    Add user data to json file
-    """
-    try:
-        services.add_userdata(user.dict())
-    except:
-        return {"success": False}
-    return {"success": True}
-
-
-@app.get("/users", response_model=UserListOut)
-async def get_users():
-    """
-    Read user data from json file
-    """
-    return services.read_usersdata()
+@app.post("/users")
+def create_user():
+    # Implementation to create a new user and store it in the JSON file
+    ...
